3916ee7746fc66cea621d8064a45412080e82bf6
saiteja <saitejakanumuri29565@gmail.com>
2021-09-15 19:09:07 +0530

    city_weather_report added

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+from prettyprinter import pprint
+
+API_Key = 'c416f544f35b07231979188b6abfdeea'  #this API_KEY is copied from the openweathermap website.
+city = input("Enter a city: ")
+base_url = 'https://api.openweathermap.org/data/2.5/weather?appid='+API_Key+'&q='+city
+weather_data = requests.get(base_url).json() #requests.get(URL/https request,parameters in {}].json()/.html() this return specific
+pprint(weather_data) #pretty printer makes json text to dictionary key-value pairs
