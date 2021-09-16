e6a415bd7510ccb9e290fc76b493dd17c1ced45f
saiteja <saitejakanumuri29565@gmail.com>
2021-09-16 22:33:14 +0530

    password generator added

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from prettyprinter import pprint
 API_Key = 'c416f544f35b07231979188b6abfdeea'  #this API_KEY is copied from the openweathermap website.
 city = input("Enter a city: ")
 base_url = 'https://api.openweathermap.org/data/2.5/weather?appid='+API_Key+'&q='+city
-weather_data = requests.get(base_url).json() #requests.get(URL/https request,parameters in {}].json()/.html() this return specific
+weather_data = requests.get(base_url).json() #requests.get(URL/https request,parameters in {}].json()/.html()[required src] this return specific
 pprint(weather_data) #pretty printer makes json text to dictionary key-value pairs
